1d90f53f9efa50624a5de0b72fd5dfda0bf421c1
Arul Mathur <arulmathur1@gmail.com>
2023-04-30 20:13:15 -0700

    Added marker to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,54 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import "animate.css";
-import TrackVisibility from "react-on-screen";
-import mapImage from "../assets/img/uganda.png";
 import React, { useRef, useEffect, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import geoJson from "./map-data.json";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1bG1hdGh1cjEiLCJhIjoiY2xoMnNxcWRkMWdjYjNzcG02YTF2cGo5YSJ9.UlYoOYufeV3wrZi_nrpmZA';
 
+function PopupContent() {
+    return (
+        <div>
+            <h3 className="popup-title" style={{ color: "black" }}>Hello World!</h3>
+            <p className="popup-body" style={{ color: "black" }}>This is some text in the popup.</p>
+        </div>
+    );
+}
+
 function Map() {
     const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const [map, setMap] = useState(null);
+    const [lng, setLng] = useState(-87.65);
+    const [lat, setLat] = useState(41.84);
+    const [zoom, setZoom] = useState(7);
 
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
+            style: 'mapbox://styles/mapbox/outdoors-v12',
             center: [lng, lat],
             zoom: zoom
         });
-    });
+
+        setMap(map);
+
+        const popupContent = ReactDOMServer.renderToString(<PopupContent />); // Render the React component as HTML
+
+        const marker = new mapboxgl.Marker()
+            .setLngLat([lng, lat])
+            .addTo(map)
+            .setPopup(new mapboxgl.Popup().setHTML(popupContent)); // Use setHTML to set the popup content
+
+        return () => {
+            map.remove();
+        };
+    }, []);
+
+
     return (
         <div>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
             <div ref={mapContainer} className="map-container" />
         </div>
     );
